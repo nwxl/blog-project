1a445f01ab64e9b33fad89bc40a0b1e5f2e016b0
nwxl <154874949+nwxl@users.noreply.github.com>
2024-02-26 09:09:08 +0000

    creating header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,48 @@
 import React from 'react'
 import { images } from '../constants'
 
+const navItemsInfo = [
+    {name: "Home"},
+    {name: "Articles"},
+    {name: "Pages"},
+    {name: "Pricing"},
+    {name: "FAQ"},
+
+] 
+
+
+const NavItem = ({name}) => {
+    return(
+        <li className='relative  group'>
+            <a href="/" className='px-4 py-2'>
+            {name}
+            </a>
+        <span className='text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100'>
+            /
+            </span>
+    </li>
+    );
+};
+
+
 const Header = () => {
-  return (
-    <section>
-        <header>
-            <div>
-                <img src={images.Logo} alt = "logo"/>
-            </div>
-            <div>
-                <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Articles</a></li>
-                    <li><a href="">Pages</a></li>
-                    <li><a href="">Pricing</a></li>
-                    <li><a href="">FAQ</a></li>
-                </ul>
-                <button>Sign in</button>
-            </div>
-        </header>
-    </section>
-  )
+    return (
+        <section>
+            <header className='container mx-auto px-5 flex justify-between py-4 items-center'>
+                <div>
+                    <img src={images.Logo} alt="logo" />
+                </div>
+                <div className='flex gap-x-9 items-center'>
+                    <ul className='flex gap-x-5 font-semibold'>
+                        {navItemsInfo.map((item) => (
+                        <NavItem key={item.name} name={item.name}/>
+                    ))}
+                    </ul>
+                    <button className='border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibol hover:bg-blue-500 hover:text-white transition-all duration-300'>Sign in</button>
+                </div>
+            </header>
+        </section>
+    )
 }
 
 export default Header
