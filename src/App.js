aaf34ba55a3d5f563f311c789c59c406ed6a1af4
nwxl <154874949+nwxl@users.noreply.github.com>
2024-03-04 08:52:50 +0000

    Responsive header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App font-opensans">
       <header className='App-header'>
               <HomePage/>
       </header>
