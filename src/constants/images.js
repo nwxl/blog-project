4a0b59f68510d6d47d07bbbe16ae06fbb59d3f67
nwxl <154874949+nwxl@users.noreply.github.com>
2024-03-12 18:18:48 +0000

    Article component

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,10 +1,13 @@
 import Logo from '../assets/Logo.svg'
 import HeroImage from "../assets/HeroImage.svg";
-
+import Post1Image from "../assets/post1.jpg"
+import PostProfileImage from "../assets/post-profile.svg"
 
 const images = {
     Logo,
     HeroImage,
+    Post1Image,
+    PostProfileImage,
 };
 
 export default images;
